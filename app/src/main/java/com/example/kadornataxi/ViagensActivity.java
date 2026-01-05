7d738f9f2991e2819182d7abb3715134432fe1aa
José Elias <joseelias.12muniz@gmail.com>
2026-01-05 14:16:57 -0300

    Add navigation between all activities
    created missing activities and implemented basic intents to validate screen navigation flow

--- app/src/main/java/com/example/kadornataxi/ViagensActivity.java
+++ app/src/main/java/com/example/kadornataxi/ViagensActivity.java
@@ -1,6 +1,5 @@
 package com.example.kadornataxi;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,34 +9,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class ViagensActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_viagens);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-    public void irSolicitacaoAcitivity(View view){
-        Intent intent = new Intent(this, SolicitacaoActivity.class);
-        startActivity(intent);
-
-    }
-
-    public void irViagensAcitivity(View view){
-        Intent intent = new Intent(this, ViagensActivity.class);
-        startActivity(intent);
-
-    }
-
-    public void irGerarRelatorioAcitivity(View view){
-        Intent intent = new Intent(this, GerarRelatorioActivity.class);
-        startActivity(intent);
-
+    public void voltarMenu(View view){
+        finish();
     }
 }
