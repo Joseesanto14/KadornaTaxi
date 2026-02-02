159376839efc7c89759c36343e95e5653487aaea
José Elias <joseelias.12muniz@gmail.com>
2026-02-01 23:48:28 -0300

    refactor: create Viagem.java getters, setters and constructor

--- app/src/main/java/com/example/kadornataxi/model/Viagem.java
+++ app/src/main/java/com/example/kadornataxi/model/Viagem.java
@@ -1,20 +1,16 @@
 package com.example.kadornataxi.model;
 
-import androidx.annotation.NonNull;
-
 import java.io.Serializable;
-import java.util.List;
-import java.util.Locale;
 
 public class Viagem implements Serializable {
     private long id;
     private String origem;
-    private String dataOrigem;
-    private String horaOrigem;
+    private String data;
+    private String hora;
     private String destino;
     private String descricao;
     private float kmsRodados;
-    private float valorViagem;
+    private float valorKms;
     private String motorista;
     private float horaEspera;
     private float valorHoraEspera;
@@ -23,13 +19,26 @@ public class Viagem implements Serializable {
 
     public Viagem() {}
 
+    public Viagem(long id, String origem, String data, String hora, String destino, String descricao, float kmsRodados, float valorKms, String motorista, float horaEspera, float valorHoraEspera, String classificacao, float valorTotal) {
+        this.id = id;
+        this.origem = origem;
+        this.data = data;
+        this.hora = hora;
+        this.destino = destino;
+        this.descricao = descricao;
+        this.kmsRodados = kmsRodados;
+        this.valorKms = valorKms;
+        this.motorista = motorista;
+        this.horaEspera = horaEspera;
+        this.valorHoraEspera = valorHoraEspera;
+        this.classificacao = classificacao;
+        this.valorTotal = valorTotal;
+    }
+
     public static String formatarDataISO8601(String data) {
         String[] partes = data.split("/");
-        String dia = partes[0];
-        String mes = partes[1];
-        String ano = partes[2];
 
-        return ano + "-" + mes + "-" + dia;
+        return partes[2] + "-" + partes[1] + "-" + partes[0];
     }
 
     public String getDiaMes() {
@@ -37,8 +46,112 @@ public class Viagem implements Serializable {
         * Receive a Date in ISO 8601 format (YYYY-MM-DD) and return the day and month in the format DD/MM (Brazilian legibility)
          */
 
-        String[] partes = getDataOrigem().split("-");
+        String[] partes = getData().split("-");
         return partes[2] + "/" + partes[1];
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getOrigem() {
+        return origem;
+    }
+
+    public void setOrigem(String origem) {
+        this.origem = origem;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public float getKmsRodados() {
+        return kmsRodados;
+    }
+
+    public void setKmsRodados(float kmsRodados) {
+        this.kmsRodados = kmsRodados;
+    }
+
+    public float getValorKms() {
+        return valorKms;
+    }
+
+    public void setValorKms(float valorKms) {
+        this.valorKms = valorKms;
+    }
+
+    public String getMotorista() {
+        return motorista;
+    }
+
+    public void setMotorista(String motorista) {
+        this.motorista = motorista;
+    }
+
+    public float getHoraEspera() {
+        return horaEspera;
+    }
+
+    public void setHoraEspera(float horaEspera) {
+        this.horaEspera = horaEspera;
+    }
+
+    public float getValorHoraEspera() {
+        return valorHoraEspera;
+    }
+
+    public void setValorHoraEspera(float valorHoraEspera) {
+        this.valorHoraEspera = valorHoraEspera;
+    }
+
+    public String getClassificacao() {
+        return classificacao;
+    }
+
+    public void setClassificacao(String classificacao) {
+        this.classificacao = classificacao;
+    }
+
+    public float getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(float valorTotal) {
+        this.valorTotal = valorTotal;
     }
 }
 
