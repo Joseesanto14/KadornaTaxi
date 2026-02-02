c0c4e3e7c4702710b1cf6d03b024e66920a2c452
José Elias <joseelias.12muniz@gmail.com>
2026-02-01 22:27:20 -0300

    refactor: create Configuracao.java getters and setters

--- app/src/main/java/com/example/kadornataxi/model/Configuracao.java
+++ app/src/main/java/com/example/kadornataxi/model/Configuracao.java
@@ -8,5 +8,43 @@ public class Configuracao {
     private String motorista;
     private String classificacaoViagemSeparada;
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public float getValorKmRodado() {
+        return valorKmRodado;
+    }
+
+    public void setValorKmRodado(float valorKmRodado) {
+        this.valorKmRodado = valorKmRodado;
+    }
+
+    public float getValorHoraEspera() {
+        return valorHoraEspera;
+    }
+
+    public void setValorHoraEspera(float valorHoraEspera) {
+        this.valorHoraEspera = valorHoraEspera;
+    }
+
+    public String getMotorista() {
+        return motorista;
+    }
+
+    public void setMotorista(String motorista) {
+        this.motorista = motorista;
+    }
+
+    public String getClassificacaoViagemSeparada() {
+        return classificacaoViagemSeparada;
+    }
+
+    public void setClassificacaoViagemSeparada(String classificacaoViagemSeparada) {
+        this.classificacaoViagemSeparada = classificacaoViagemSeparada;
+    }
 }
